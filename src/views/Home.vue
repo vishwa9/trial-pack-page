6342253077627c5a6df03ed7b154f9416db4774d
vishwajeet singh chauhan <vishwajeet11104736@gmail.com>
2019-06-30 15:17:37 +0530

    screen made responsive for low resolution

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,31 +7,31 @@
       <router-link to="/one">
         <div class="flex flex-dir-col align-item">
           <div>1</div>
-          <div>(2-3) KG</div>
+          <div class="text-inline">(2-3) KG</div>
         </div>
       </router-link>
       <router-link to="/two">
         <div class="flex flex-dir-col align-item">
           <div>2</div>
-          <div>(3-4) KG</div>
+          <div class="text-inline">(3-4) KG</div>
         </div>
       </router-link>
       <router-link to="/three">
         <div class="flex flex-dir-col align-item">
           <div>3</div>
-          <div>(4-7) KG</div>
+          <div class="text-inline">(4-7) KG</div>
         </div>
       </router-link>
       <router-link to="/four">
         <div class="flex flex-dir-col align-item">
           <div>4</div>
-          <div>(7-10) KG</div>
+          <div class="text-inline">(7-10) KG</div>
         </div>
       </router-link>
       <router-link to="/five">
         <div class="flex flex-dir-col align-item">
           <div>5</div>
-          <div>(10-12) KG</div>
+          <div class="text-inline">(10-12) KG</div>
         </div>
       </router-link>
     </div>
